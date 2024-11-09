288a14f6b3ca4783a3ab8aa72478258a7a2b0aac
HGUFlyingDog <suidonghui666@gmail.com>
2024-11-09 21:52:00 +0800

    9以内进制转换

--- 实验02 栈与队列结构及其应用/conversion.cpp
+++ 实验02 栈与队列结构及其应用/conversion.cpp
@@ -1,14 +1,14 @@
 //5.	实现对用户输入的十进制数转化成其他进制并输出
 #include"stack.h"
-void conversion(int N)
+void conversion(int N,int num)
 {
 	LinkStack S;
 	InitStack(S);
 	int e;
 	while (N)
 	{
-		StackPush(S, N % 8);
-		N = N / 8;
+		StackPush(S, N % num);
+		N = N / num;
 	}
 	
 	while (!StackEmpty(S))
