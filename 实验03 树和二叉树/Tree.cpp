0506d8278d197d271f6eb70486fb4c1f7d4084dc
HGUFlyingDog <suidonghui666@gmail.com>
2024-11-14 15:08:20 +0800

    更新CreateBiTree

--- 实验03 树和二叉树/Tree.cpp
+++ 实验03 树和二叉树/Tree.cpp
@@ -1,16 +1,19 @@
 #include"Tree.h"
 
-void CreateBiTree(BiTree& T) 
+void CreateBiTree(BiTree& T)
 {
-    char ch;
-    cin >> ch;
-    if (ch == '#') {
-        T = nullptr;  // 递归结束，建空树
-    }
-    else {
-        T = new BiTNode;  // 生成根节点
-        T->data = ch;  // 根节点数据域置为ch
-        CreateBiTree(T->lchild);  // 递归创建左子树
-        CreateBiTree(T->rchild);  // 递归创建右子树
-    }
+	char ch;
+	cin >> ch;
+	if (ch == '#')
+		return;
+	else
+	{
+		T = new BiTNode;
+		T->data = ch;
+		CreateBiTree(T->lchild);
+		CreateBiTree(T->rchild);
+		return;
+	}
+
+	return;
 }
